e780de5934aa2dc40b1e42af44517c8d9939dcc9
Lucas5236 <Albasri@live.com>
2015-07-18 20:00:49 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1268,7 +1268,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["World Eater"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
